188d542781f343c2cde26b13627ad7cdc6128352
cwschopsjr <cwschops.jr@gmail.com>
2024-05-21 15:12:44 -0300

    Configurando o POST

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -28,7 +28,47 @@ def index(request):
     )
 
 
-def page(request):
+def created_by(request, author_pk):
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True, created_by__pk=author_pk, )
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def category(request, slug):
+    posts = (
+        Post
+        .objects
+        .filter(is_published=True, category__slug=slug, )
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+
+def page(request, slug):
 
     return render(
         request,
@@ -39,12 +79,19 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+
+    post = (
+        Post
+        .objects
+        .filter(is_published=True, slug=slug, )
+        .first()
+    )
 
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
